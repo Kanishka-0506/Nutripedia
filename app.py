2743220435384046f43fd6b781630ebde4dc2fee
Varsaa-10 <146560391+Varsaa-10@users.noreply.github.com>
2025-06-11 13:36:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -121,7 +121,11 @@ def predict():
 
     return jsonify(response)
 
+# if __name__ == '__main__':
+  #  import os
+   # port=int(os.environ.get("PORT",5000))
+    # app.run(host='0.0.0.0',port=port)
+# Only needed if you want to run locally
 if __name__ == '__main__':
-    import os
-    port=int(os.environ.get("PORT",5000))
-    app.run(host='0.0.0.0',port=port)
+    app.run(debug=True)
+
